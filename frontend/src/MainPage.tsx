1bfc53b8057ed449c508fb2699197c659627550c
Richard Bilson <rcbilson@gmail.com>
2025-06-07 22:20:03 -0400

    no auth

--- frontend/src/MainPage.tsx
+++ frontend/src/MainPage.tsx
@@ -1,4 +1,3 @@
-import { RequireAuth } from "@/components/ui/RequireAuth";
 import { Tabs } from '@chakra-ui/react'
 import { LuBookmarkPlus, LuStar, LuClock, LuSearch } from "react-icons/lu"
 import { useLocation, Link } from "react-router-dom"
@@ -13,48 +12,46 @@ export default function MainPage() {
   const activeTab = location.pathname.split('/')[1] || 'recent';
 
   return (
-    <RequireAuth>
-        <Tabs.Root defaultValue="favorites" variant="line"
-            value={activeTab} onChange={() => {}}>
-          <Tabs.List>
-            <Tabs.Trigger value="recent">
-              <LuClock />
-              <Link to="/recent">
-                Recent
-              </Link>
-            </Tabs.Trigger>
-            <Tabs.Trigger value="archive">
-              <LuStar />
-              <Link to="/archive">
-                Archive
-              </Link>
-            </Tabs.Trigger>
-            <Tabs.Trigger value="search">
-              <LuSearch />
-              <Link to="/search">
-                Search
-              </Link>
-            </Tabs.Trigger>
-            <Tabs.Trigger value="add">
-              <LuBookmarkPlus />
-              <Link to="/add">
-                Add
-              </Link>
-            </Tabs.Trigger>
-          </Tabs.List>
-          <Tabs.Content value="archive">
-            <ArchivePage />
-          </Tabs.Content>
-          <Tabs.Content value="recent">
-            <RecentPage />
-          </Tabs.Content>
-          <Tabs.Content value="search">
-            <SearchPage />
-          </Tabs.Content>
-          <Tabs.Content value="add">
-            <AddPage />
-          </Tabs.Content>
-        </Tabs.Root>
-    </RequireAuth>
+    <Tabs.Root defaultValue="favorites" variant="line"
+      value={activeTab} onChange={() => { }}>
+      <Tabs.List>
+        <Tabs.Trigger value="recent">
+          <LuClock />
+          <Link to="/recent">
+            Recent
+          </Link>
+        </Tabs.Trigger>
+        <Tabs.Trigger value="archive">
+          <LuStar />
+          <Link to="/archive">
+            Archive
+          </Link>
+        </Tabs.Trigger>
+        <Tabs.Trigger value="search">
+          <LuSearch />
+          <Link to="/search">
+            Search
+          </Link>
+        </Tabs.Trigger>
+        <Tabs.Trigger value="add">
+          <LuBookmarkPlus />
+          <Link to="/add">
+            Add
+          </Link>
+        </Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="archive">
+        <ArchivePage />
+      </Tabs.Content>
+      <Tabs.Content value="recent">
+        <RecentPage />
+      </Tabs.Content>
+      <Tabs.Content value="search">
+        <SearchPage />
+      </Tabs.Content>
+      <Tabs.Content value="add">
+        <AddPage />
+      </Tabs.Content>
+    </Tabs.Root>
   )
 }
